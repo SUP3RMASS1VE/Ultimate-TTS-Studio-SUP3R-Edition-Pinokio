18ee1fea88eb494b667a1121a1a5853fbfc2e293
SUP3RMASS1VE <Martincull@live.com>
2025-09-18 16:06:52 +0100

    Update install.js

--- install.js
+++ install.js
@@ -1,6 +1,5 @@
 module.exports = {
   run: [
-    // Edit this step to customize the git repository to use
     {
       method: "shell.run",
       params: {
@@ -9,34 +8,31 @@ module.exports = {
         ]
       }
     },
-    // Delete this step if your project does not use torch
-    {
-      method: "script.start",
-      params: {
-        uri: "torch.js",
-        params: {
-          conda: "tts_env",                // Edit this to customize the conda folder path
-          path: "app",                // Edit this to customize the path to start the shell from
-          // xformers: true   // uncomment this line if your project requires xformers
-          // triton: true   // uncomment this line if your project requires triton
-          // sageattention: true   // uncomment this line if your project requires sageattention
-        }
-      }
-    },
-    // Edit this step with your custom install commands
     {
       method: "shell.run",
       params: {
-        conda: "tts_env",                // Edit this to customize the conda folder path
-        path: "app",                // Edit this to customize the path to start the shell from
+        conda: "tts_env",
+        path: "app",
         message: [
           "conda install -c conda-forge pynini==2.1.6 -y",
           "uv pip install gradio devicetorch",
           "uv pip install -r requirements.txt",
           "uv pip install WeTextProcessing --no-deps",
           "pip uninstall phonemizer-fork -y",
-          "pip install phonemizer-fork"
+          "pip install phonemizer-fork",
+          "uv pip install --upgrade --force-reinstall --no-deps --no-cache-dir onnxruntime-gpu==1.22.0"
         ]
+      }
+    },
+    {
+      method: "script.start",
+      params: {
+        uri: "torch.js",
+        params: {
+          conda: "tts_env",
+          path: "app",
+          triton: true
+        }
       }
     },
     {
@@ -47,11 +43,6 @@ module.exports = {
         "--local-dir": "./checkpoints/openaudio-s1-mini",
       }
     },
-
-
-       
-    // espeak-ng installer script lifted from AllTalk Launcher from 6Morpheus6
-    // https://github.com/pinokiofactory/AllTalk-TTS/blob/main/install.js
     {
       when: "{{which('brew')}}",
       method: "shell.run",
@@ -96,3 +87,4 @@ module.exports = {
     },
   ]
 }
+
